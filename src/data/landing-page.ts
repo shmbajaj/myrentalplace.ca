2567425936078938239c0ab3ba9ae410d1826493
shubham bajaj <shmbajaj32@gmail.com>
2023-05-07 18:30:59 +0530

    feat: landing page

---
+++ src/data/landing-page.ts
@@ -0,0 +1,73 @@
+interface PopularPlace {
+  id: string;
+  href: string;
+  src: string;
+  text: string;
+  alt: string;
+}
+
+export const POPULAR_PLACES: Array<PopularPlace> = [
+  {
+    id: 'some-random-id-1',
+    href: '#',
+    src: 'https://80fb29193e49ee2fd504-d0f6d041df1ee747e1fc6df6df0d002b.ssl.cf1.rackcdn.com/timg-tile-toronto.jpg',
+    alt: 'Toronto City',
+    text: 'Toronto',
+  },
+  {
+    id: 'some-random-id-2',
+    href: '#',
+    src: 'https://80fb29193e49ee2fd504-d0f6d041df1ee747e1fc6df6df0d002b.ssl.cf1.rackcdn.com/timg-tile-toronto.jpg',
+    alt: 'Ontario City',
+    text: 'Ontario',
+  },
+  {
+    id: 'some-random-id-3',
+    href: '#',
+    src: 'https://80fb29193e49ee2fd504-d0f6d041df1ee747e1fc6df6df0d002b.ssl.cf1.rackcdn.com/timg-tile-regina.jpg',
+    alt: 'Regina City',
+    text: 'Regina',
+  },
+  {
+    id: 'some-random-id-1',
+    href: '#',
+    src: 'https://80fb29193e49ee2fd504-d0f6d041df1ee747e1fc6df6df0d002b.ssl.cf1.rackcdn.com/timg-tile-toronto.jpg',
+    alt: 'Toronto City',
+    text: 'Toronto',
+  },
+  {
+    id: 'some-random-id-2',
+    href: '#',
+    src: 'https://80fb29193e49ee2fd504-d0f6d041df1ee747e1fc6df6df0d002b.ssl.cf1.rackcdn.com/timg-tile-toronto.jpg',
+    alt: 'Ontario City',
+    text: 'Ontario',
+  },
+  {
+    id: 'some-random-id-3',
+    href: '#',
+    src: 'https://80fb29193e49ee2fd504-d0f6d041df1ee747e1fc6df6df0d002b.ssl.cf1.rackcdn.com/timg-tile-regina.jpg',
+    alt: 'Regina City',
+    text: 'Regina',
+  },
+  {
+    id: 'some-random-id-1',
+    href: '#',
+    src: 'https://80fb29193e49ee2fd504-d0f6d041df1ee747e1fc6df6df0d002b.ssl.cf1.rackcdn.com/timg-tile-toronto.jpg',
+    alt: 'Toronto City',
+    text: 'Toronto',
+  },
+  {
+    id: 'some-random-id-2',
+    href: '#',
+    src: 'https://80fb29193e49ee2fd504-d0f6d041df1ee747e1fc6df6df0d002b.ssl.cf1.rackcdn.com/timg-tile-toronto.jpg',
+    alt: 'Ontario City',
+    text: 'Ontario',
+  },
+  {
+    id: 'some-random-id-3',
+    href: '#',
+    src: 'https://80fb29193e49ee2fd504-d0f6d041df1ee747e1fc6df6df0d002b.ssl.cf1.rackcdn.com/timg-tile-regina.jpg',
+    alt: 'Regina City',
+    text: 'Regina',
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,80 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
+import Header from '@/components/header/header';
+import ImageCard from '@/components/image-card/image-card';
+import Footer from '@/components/footer/footer';
+import { POPULAR_PLACES } from '@/data/landing-page';
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <Head>
+        <title>My Rental Place | House For Rent In Canada</title>
+      </Head>
+      <Header />
+      <main>
+        <section className="bg-gray-50">
+          <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+            <div className="mx-auto max-w-lg text-center">
+              <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
+                House for Rent in Canada.
+              </h2>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <p className="hidden text-gray-500 sm:mt-4 sm:block">
+                Canada&apos;s largest network of houses for rent.
+              </p>
+            </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+            <div className="mx-auto mt-8 max-w-xl">
+              <form>
+                <div className="relative">
+                  <label htmlFor="PropertySearch" className="sr-only">
+                    Search...
+                  </label>
+
+                  <input
+                    type="text"
+                    id="PropertySearch"
+                    placeholder="Search..."
+                    className="w-full rounded-md border-gray-200 py-2.5 px-2.5 shadow-sm sm:text-sm"
+                  />
+
+                  <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+                    <button
+                      type="button"
+                      className="rounded-full bg-primary-500 p-0.5 text-white hover:bg-primary-600"
+                    >
+                      <span className="sr-only">Submit</span>
+                      <svg
+                        className="h-5 w-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          clipRule="evenodd"
+                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                          fillRule="evenodd"
+                        ></path>
+                      </svg>
+                    </button>
+                  </span>
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
+        <section className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="mt-2 text-2xl font-bold text-gray-900 md:text-3xl">
+            Popular Places
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
+            {POPULAR_PLACES.map(({ id, ...rest }) => (
+              <ImageCard key={id} {...rest} />
+            ))}
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 }
